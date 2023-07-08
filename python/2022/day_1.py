2ce38b937a244ff50cdc4610ba5cdaf0ba5f285b
Matias Bertani <matiasbertani@gmail.com>
2023-07-08 18:53:47 -0300

    python: 2022: resolve day 1

---
+++ python/2022/day_1.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+def get_calories_from_elf_with_most_calories(input_filepath: Path) -> int:
+    all_calories = get_all_calories_from_file(input_filepath)
+    amount_of_calories_per_elf = [
+        sum(elf_calories)
+        for elf_calories in group_calories_by_elf(all_calories)
+    ]
+    return max(amount_of_calories_per_elf)
+
+
+def get_all_calories_from_file(filepath: Path) -> list[str]:
+    with open(filepath) as f:
+        data = f.readlines()
+    return data
+
+
+def group_calories_by_elf(all_elf_colories: list) -> list[int]:
+    one_elf_calories = []
+    for element in all_elf_colories:
+        element = element.strip()
+        if not element:
+            yield one_elf_calories
+            one_elf_calories = []
+            continue
+        one_elf_calories.append(int(element))
+    yield one_elf_calories
+
+
+if __name__ == "__main__":
+
+    current_dir = Path(__file__).parent
+    input_filepath = current_dir / '../../inputs/2022/day_1.txt'
+    max_calories = get_calories_from_elf_with_most_calories(input_filepath)
+    print(max_calories)
