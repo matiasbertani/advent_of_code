460d21175c66ae29b6cfc6374c2b11868f78e7da
Matias Bertani <matiasbertani@gmail.com>
2023-07-30 23:00:43 -0300

    python/2022: add part two of day 1

--- python/2022/day_1.py
+++ python/2022/day_1.py
@@ -33,5 +33,5 @@ if __name__ == "__main__":
 
     current_dir = Path(__file__).parent
     input_filepath = current_dir / '../../inputs/2022/day_1.txt'
-    max_calories , sum_of_top_thre_calories = get_calories_from_elf_with_most_calories(input_filepath)
+    max_calories, sum_of_top_thre_calories = get_calories_from_elf_with_most_calories(input_filepath)
     print(max_calories, sum_of_top_thre_calories)
